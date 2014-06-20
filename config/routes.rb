3b046f55d6543b32d0315878d7dc84bc5788e850
andy chen <chen.a.andy@gmail.com>
2014-06-19 21:58:25 -0700

    added login, logout, destroy session. Wasn't able to get flash notices to work properly in the sessions controller to work.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   get '/' => 'main#start'
   get '/new_user' => 'users#new'
   post '/new_user' => 'users#new_user'
+  
+  #for login/logout  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 end
