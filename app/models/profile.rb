ab7988ab6de9d4bf0c2c3875fd6a36b8d9415a42
Jose Juarez <120photo@gmail.com>
2014-06-23 18:48:44 -0700

    added image upload to Profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,10 @@
 class Profile
 	Mongoid::Document
+	include Mongoid::Paperclip
+
+	has_mongoid_attached_file :image
+	validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
+
 
 	field :user_name, type: String # this should be a users real name, not a uniq login name
 	field :bio, type: String # short profile description
