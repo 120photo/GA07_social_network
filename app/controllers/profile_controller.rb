86b67ce8842cf21477de0239b53d94eb20308205
andy chen <chen.a.andy@gmail.com>
2014-06-24 16:04:10 -0700

    made profile controller show array profile instance grab only one profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,7 +6,7 @@ class ProfileController < ApplicationController
 		@current_user_profile = User.find(params[:id])
 		@email = @current_user_profile.email
 		@user_id = @current_user_profile.id
-	    @current_profile = Profile.where(user_id: params[:id])
+	    @current_profile = Profile.where(user_id: params[:id]).first
 	    # @current_profile = Profile.find(user_id: params[:@user_id])
 	    # @user = @current_profile.user_name
 		# @profile = Profile.all # pulls all user profiles
