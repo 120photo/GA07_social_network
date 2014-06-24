0d4e6d1a4c45fff1324687acf15b9c64dc30560b
Jose Juarez <120photo@gmail.com>
2014-06-23 20:22:44 -0700

    crate profile template and controller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,7 +1,17 @@
 class ProfileController < ApplicationController
 	def index
 	end
-	def show
 
+	def show
+	end
+
+	def create
+		profile = Profile.new
+
+		profile.user_name = params[:profile][:user_name]
+		profile.bio = params[:profile][:bio]
+		profile.image = params[:profile][:image]
+		profile.save!
+		redirect_to '/'
 	end
 end
