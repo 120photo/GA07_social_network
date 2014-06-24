54090d1b37655a027a5ca60fbc50c03d5590d645
Jose Juarez <120photo@gmail.com>
2014-06-23 19:24:28 -0700

    changed create_profile get route to use empty index method

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   #for profile
   get '/profile/:id' => 'profile#index'
-  get '/create_profile' => 'profile#new'
+  get '/create_profile' => 'profile#index'
   post '/create_profile' => 'profile#create'
 
 
